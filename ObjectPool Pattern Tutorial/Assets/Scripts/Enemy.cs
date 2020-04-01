7225a9f16ed6898103ed307547aff9ff1e3bff01
cxz5309 <cxz5309@gmail.com>
2020-04-01 16:36:03 +0900

    no message

---
+++ ObjectPool Pattern Tutorial/Assets/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ ObjectPool Pattern Tutorial/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public GameObject enemy;
+    public int poolSize = 10;
+    GameObject[] enemyPool;
+
+    private void Start()
+    {
+        enemyPool = new GameObject[poolSize];
+        for (int i = 0; i < poolSize; i++) {
+            enemyPool[i] = Instantiate(enemy) as GameObject;
+            enemyPool[i].name = "Enemy_" + i;
+            enemyPool[i].SetActive(false);
+        }
+        StartCoroutine("SpawnManager");
+    }
+
+    IEnumerator SpawnManager()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(2.0f);
+            for(int i = 0; i < poolSize; i++)
+            {
+                if (enemyPool[i].activeSelf == true)
+                    continue;
+
+                float x = Random.Range(-5, 6);
+                float z = Random.Range(-5, 6);
+
+                enemyPool[i].transform.position = new Vector3(x, 0.5f, z);
+                enemyPool[i].SetActive(true);
+
+                break;
+            }
+        }
+    }
+}
